cbd58a9cdc48c3f69cd7778bf41c2b1fdbd8bbcb
prc <prcolaco@gmail.com>
2024-03-15 15:36:04 +0000

    remove hf 480

--- tests/common/database_fixture.cpp
+++ tests/common/database_fixture.cpp
@@ -1167,8 +1167,7 @@ void database_fixture_base::publish_feed( const asset_object& mia, const account
    if( op.feed.core_exchange_rate.is_null() )
    {
       op.feed.core_exchange_rate = op.feed.settlement_price;
-      if( db.head_block_time() > HARDFORK_480_TIME )
-         op.feed.core_exchange_rate.quote.asset_id = asset_id_type();
+      op.feed.core_exchange_rate.quote.asset_id = asset_id_type();
    }
    trx.operations.emplace_back( std::move(op) );
 
